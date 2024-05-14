5120cf986bf57faeb014791d02b2ddd0ca6b919a
Justas Remeikis <justas00001@gmail.com>
2024-05-14 20:01:53 +0300

    chore: Add monitor speed and filters configuration, and implement LED blinking in main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,27 @@
-void app_main() {}
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#define LED_BUILD_IN 2
+
+void app_main(void) 
+{
+    char *taskName = pcTaskGetName(NULL);
+    ESP_LOGI(taskName, "Hello, staring up!\n");
+
+    gpio_reset_pin(LED_BUILD_IN);
+    gpio_set_direction(LED_BUILD_IN, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_BUILD_IN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_BUILD_IN, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
